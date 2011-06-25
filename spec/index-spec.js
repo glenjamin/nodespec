fe460781b377cacb689ba210522a13d206d36993
Glen Mailer <glenjamin@gmail.com>
2011-06-25 20:16:34 +0100

    specs for nodespec.require

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -30,8 +30,8 @@ nodespec.describe("Nodespec", function() {
         });
     });
     this.context("sandboxed nodespec module", function() {
-        this.subject("nodespec", function() {
-            return nodespec("fresh nodespec to test");
+        this.before("nodespec", function() {
+            this.nodespec = nodespec("fresh nodespec to test");
         });
         this.after(function() {
             this.nodespec.abandon();
@@ -69,6 +69,8 @@ nodespec.describe("Nodespec", function() {
                 this.assert.equal(typeof call.args[1], 'object');
                 // options.parent is nodespec
                 this.assert.strictEqual(call.args[1].parent, this.nodespec);
+                // options.nodespec is nodespec
+                this.assert.strictEqual(call.args[1].nodespec, this.nodespec);
                 // Arg 3 = definition function
                 this.assert.equal(call.args[2], this.definition);
             });
@@ -83,8 +85,9 @@ nodespec.describe("Nodespec", function() {
                 // Arg 2 = options object
                 this.assert.strictEqual(call.args[1], options);
                 // options.parent is still set to nodespec
-                this.assert.strictEqual(options.parent, this.nodespec);
-
+                this.assert.strictEqual(call.args[1].parent, this.nodespec);
+                // options.nodespec is still set to nodespec
+                this.assert.strictEqual(call.args[1].nodespec, this.nodespec);
             });
             this.example("appends new instance to collection", function() {
                 var group = this.nodespec.describe("a set of tests",
@@ -92,6 +95,26 @@ nodespec.describe("Nodespec", function() {
                 this.assert.strictEqual(this.nodespec.example_groups.length, 1);
                 this.assert.strictEqual(this.nodespec.example_groups[0], group);
             })
+        });
+        this.describe("require", function() {
+            this.example("should call native require", function() {
+                var Module = require('module').Module;
+                var load = this.sinon.stub(Module, "_load");
+
+                this.nodespec.require("some-tests");
+                this.sinon.assert.calledOnce(load);
+                this.sinon.assert.calledWith(load, __dirname + "/some-tests");
+            });
+            this.example("should disable exec during the require", function() {
+                var Module = require('module').Module;
+                var load = this.sinon.stub(Module, "_load", function() {
+                    this.nodespec.exec();
+                }.bind(this));
+                var exec = this.sinon.stub(this.nodespec, "exec");
+
+                this.nodespec.require("some-tests");
+                this.sinon.assert.notCalled(exec);
+            });
         });
     });
 });
